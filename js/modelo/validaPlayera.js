31a0fa79e6f8cfbfacdeea440084d3b08f15d3c0
YANITZINMARTINEZBASILIO <151480753+YANITZINMARTINEZBASILIO@users.noreply.github.com>
2024-11-25 16:50:17 -0600

    commit

--- js/modelo/validaPlayera.js
+++ js/modelo/validaPlayera.js
@@ -4,21 +4,27 @@
  */
 export function validaPlayera(objeto) {
 
- if (typeof objeto.PLA_ID !== "string")
-  throw new Error("El id debe ser texto.")
-
- if (typeof objeto.PLA_NOM !== "string")
-  throw new Error("El nombre debe ser texto.")
-
- if (typeof objeto.PLA_TALLA !== "string")
-    throw new Error("La talla debe ser texto.")
-
- if (typeof objeto.PLA_TELA !== "string")
-    throw new Error("La Tela debe ser texto.")
-
- if (typeof objeto.PLA_COLOR !== "string")
-    throw new Error("El color debe ser texto.")
-
- return objeto
-
-}
+   if (typeof objeto.PLA_ID !== "string") {
+     throw new Error("El id debe ser texto.");
+   }
+ 
+   if (typeof objeto.PLA_NOM !== "string") {
+     throw new Error("El nombre debe ser texto.");
+   }
+ 
+   if (typeof objeto.PLA_TALLA !== "string") {
+     console.log("Valor de PLA_TALLA:", objeto.PLA_TALLA); // Depura el valor de PLA_TALLA
+     throw new Error("La talla debe ser texto.");
+   }
+ 
+   if (typeof objeto.PLA_TELA !== "string") {
+     throw new Error("La Tela debe ser texto.");
+   }
+ 
+   if (typeof objeto.PLA_COLOR !== "string") {
+     throw new Error("El color debe ser texto.");
+   }
+ 
+   return objeto;
+ }
+ 
--- lib/php/recuperaJson.php
+++ lib/php/recuperaJson.php
@@ -1,6 +1,11 @@
 <?php
 
-function recuperaJson()
-{
- return json_decode(file_get_contents("php://input"));
+function recuperaJson() {
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+    if ($data === null) {
+        throw new Exception("Error al decodificar JSON");
+    }
+    return $data;
 }
+
